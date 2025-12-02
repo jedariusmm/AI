e3a67522fe38db7861eab8a99426a606a73b333f
Jedarius Maxwell <Jedarius.m@yahoo.com>
2025-12-02 02:37:41 -0500

    Add test API endpoint to verify routing

---
+++ nupiai-deploy/api/test.js
@@ -0,0 +1,7 @@
+// Simple test endpoint
+module.exports = (req, res) => {
+    res.status(200).json({ 
+        message: 'API is working!',
+        timestamp: new Date().toISOString() 
+    });
+};
